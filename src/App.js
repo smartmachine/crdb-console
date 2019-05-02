792ee7b4d618e1df6f75e63648326b94bcb4c52f
Conrad Christensen <conrad.christensen@gmail.com>
2019-05-02 14:49:56 +0200

    CockroachDB React Webapp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,91 @@
 import React, { Component } from 'react';
-import './App.css';
-import {Card, Container, Grid, Header, Icon, Image, List, Menu} from "semantic-ui-react";
-import logo from './assets/logo.svg'
+
+import {Button, Grid, Header, Icon, Image, Label, Menu, Segment} from "semantic-ui-react";
+
 import Websocket from "./Websocket";
+
+import { Terminal } from 'xterm';
+import * as fullScreen from 'xterm/lib/addons/fullscreen/fullscreen';
+
+import logo from './assets/logo.svg'
+
+import 'xterm/dist/xterm.css'
+import 'xterm/lib/addons/fullscreen/fullscreen.css';
+
+import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      clusters: {},
+      currentDatabase: undefined,
+      log: []
+    }
+  }
+
   handleData = (data) => {
-    console.log(JSON.parse(data));
-    this.websocket.sendMessage(JSON.stringify({
-      text: "Back at ya!"
-    }))
+    let record = JSON.parse(data);
+    let clusters = this.state.clusters;
+    let log = this.state.log;
+    let { key, crud } = record;
+    let newState = {};
+    if (this.state.currentDatabase) {
+      newState['currentDatabase'] = this.state.currentDatabase;
+    }
+    if (crud === 'record') {
+      clusters[key] = record['data'];
+      log.unshift(record['data']['status']['state']);
+    }
+    if (crud === 'delete') {
+      delete clusters[key];
+      if (this.state.currentDatabase === key) {
+        newState['currentDatabase'] = undefined;
+      }
+      log = []
+    }
+    newState['clusters'] = clusters;
+    newState['log'] = log;
+    if (!newState.currentDatabase && Object.keys(clusters).length > 0) {
+      newState.currentDatabase = Object.keys(clusters)[0]
+    }
+    this.setState(newState);
+    console.log(this.state)
   };
 
+  handleOpen = () => {
+    //this.websocket.sendMessage(JSON.stringify({cmd: "detail", key: "crdb/crdb-test"}))
+  };
+
+  termOpen = (event, data) => {
+      if (data.activeIndex === 2) {
+        this.term = new Terminal();
+        this.term.open(document.getElementById('terminal'));
+        this.term.write('Hello from react');
+      } else if (this.term) {
+        this.term.dispose();
+      }
+  };
+
+  selectDatabase = (name) => {
+    this.websocket.sendMessage(JSON.stringify({cmd: "detail", key: name}));
+    this.setState({ currentDatabase: name})
+  };
+
+  componentDidMount() {
+    Terminal.applyAddon(fullScreen);
+  }
+
+
   render() {
+
+    let { clusters, currentDatabase, log } = this.state;
+    let nodes = [];
+    if (currentDatabase && clusters[currentDatabase]['status']['nodes']) {
+      nodes = clusters[currentDatabase]['status']['nodes']
+    }
+
     return (
       <div>
         <Menu fixed='top'>
@@ -29,33 +101,43 @@ class App extends Component {
             Create
           </Menu.Item>
         </Menu>
-        <Grid id="content">
-          <Grid.Column width={4}>
-            <Card>
-              <Card.Content>
-                <Card.Header>Clusters</Card.Header>
-              </Card.Content>
-              <Card.Content>
-                <List>
-                  <List.Item>
-                    crdb-apps
-                  </List.Item>
-                  <List.Item>
-                    crdb-infra
-                  </List.Item>
-                </List>
-              </Card.Content>
-            </Card>
+        <Grid celled style={{height: '95vh'}}>
+          <Grid.Column width={2} textAlign="left">
+            <Header as="h2">
+              Clusters
+            </Header>
+            {Object.keys(clusters).map((item, index) => (
+              <Label as='a' color='blue' key={index} onClick={() => this.selectDatabase(item)} image>
+                <Icon name="bolt"/>
+                {item.split('/')[0]}
+                <Label.Detail>{item.split('/')[1]}</Label.Detail>
+              </Label>
+            ))}
           </Grid.Column>
-          <Grid.Column width={12}>
-            <Container>
-              <Header as='h4'>
-                Detail
+          <Grid.Column width={14}>
+            <Grid.Row style={{height: '70%'}}>
+              <Header as="h2">
+                {currentDatabase}
               </Header>
-            </Container>
+              {nodes.length > 0 ? nodes.map((item, index) => (<p key={index}>{item.name}</p>)) : (<p/>) }
+              <br/>
+            </Grid.Row>
+            <Grid.Row style={{height: '30%'}}>
+              <Segment.Group style={{height: '100%'}}>
+                <Segment>
+                  <Button compact size='small' floated='right' onClick={this.clearLog}>
+                    Clear
+                  </Button>
+                  Event Log <Label circular>{log.length}</Label>
+                </Segment>
+                <Segment secondary style={{ overflow: 'auto', maxHeight: '70%'}} hidden={log.length === 0}>
+                  <pre>{log.map((e, i) => <div key={i}>{e}</div>)}</pre>
+                </Segment>
+              </Segment.Group>
+            </Grid.Row>
           </Grid.Column>
         </Grid>
-        <Websocket url='ws://localhost:8080/ws' onMessage={this.handleData} ref={(Websocket) => {this.websocket = Websocket}}/>
+        <Websocket url='ws://localhost:8080/ws' onMessage={this.handleData} onOpen={this.handleOpen} ref={(Websocket) => {this.websocket = Websocket}}/>
       </div>
     );
   }
